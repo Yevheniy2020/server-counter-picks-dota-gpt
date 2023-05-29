4e518629b05116daf07bf9788167971f9c3a1887
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2023-05-29 15:42:57 +0200

    Update chat.js

--- services/chat.js
+++ services/chat.js
@@ -2,18 +2,29 @@ import axios from "axios";
 
 export class ChatGPT {
 
-    options = {
-        method: 'GET',
-        url: 'https://openai80.p.rapidapi.com/models',
-        headers: {
-            'X-RapidAPI-Key': '2e7fb0a875msh6f7dcc75f08eefap16e088jsn4243fe649b4a',
-            'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
-        }
-    };
+    getAnswer = async (question) => {
 
-    getAnswer = async (q) => {
+        const options = {
+            method: 'POST',
+            url: 'https://openai80.p.rapidapi.com/chat/completions',
+            headers: {
+                'content-type': 'application/json',
+                'X-RapidAPI-Key': '2e7fb0a875msh6f7dcc75f08eefap16e088jsn4243fe649b4a',
+                'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
+            },
+            data: {
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        role: 'user',
+                        content: `Now you're a Dota 2 e-player, and you have to write counterpicks for ${question}`
+                    }
+                ]
+            }
+        };
+
         try {
-            const response = await axios.request(this.options);
+            const response = await axios.request(options);
             return response.data
         } catch (error) {
             console.error(error);
